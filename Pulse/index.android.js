e19f1d79951aa9a922390931a5493b62e74e176d
poojasethi <sethi.pooja@gmail.com>
2016-02-13 15:18:42 -0800

    Can click buttons

--- Pulse/index.android.js
+++ Pulse/index.android.js
@@ -10,22 +10,72 @@ var {
   StyleSheet,
   Text,
   View,
+  Navigator,
 } = React;
+
+// var MySceneComponent = React.createClass({
+//   render: function() {
+//     return (
+//       <View>
+//         <Text onClick={}>hello</Text>
+//       </View>
+//     );
+//   }
+// });
+var Button = require('react-native-button');
+var React = require('react-native');
+
+var MySceneComponent = React.createClass({
+  getInitialState: function() {
+    return {
+      buttonText: this.props.buttonText
+    };
+  },
+
+  render: function() {
+    return (
+      <Button
+        style={{fontSize: 100, color: 'green'}}
+        styleDisabled={{color: 'red'}}
+        onPress={this._handlePress}
+      >
+        {this.state.buttonText}
+      </Button>
+    );
+  },
+
+  _handlePress: function(event) {
+    this.setState({
+      buttonText: 'clicked'
+    })
+  },
+});
+
 
 var EagleEye = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Navigator
+        initialRoute={{name: 'My First Scene', index: 0}}
+        renderScene={(route, navigator) =>
+          <MySceneComponent
+            name={route.name}
+            buttonText={'hello there'}
+            onForward={() => {
+              var nextIndex = route.index + 1;
+              navigator.push({
+                name: 'Scene ' + nextIndex,
+                index: nextIndex,
+              });
+            }}
+            onBack={() => {
+              if (route.index > 0) {
+                navigator.pop();
+              }
+            }}
+          />
+        }
+      />
     );
   }
 });
@@ -38,6 +88,7 @@ var styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
+    color: 'red',
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
